451c875b462cde6d292cef6610a1353e1cd73bd0
thendray <panda8o7@yandex.ru>
2022-12-02 18:41:18 +0300

    реализация методов интерфейса

--- hw_sem/hw3/src/Main.java
+++ hw_sem/hw3/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyList myList = new MyList();
+        Integer num1 = 5;
+
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.print();
+
+        myList.add(4);
+        myList.add(5);
+        myList.print();
+
+        myList.remove(0);
+        System.out.println(myList.indexOf(num1));
+        myList.remove(num1);
+        myList.print();
+
+        myList.remove(myList.get(2));
+        myList.print();
     }
 }
--- hw_sem/hw3/src/MyList.java
+++ hw_sem/hw3/src/MyList.java
@@ -1,46 +1,135 @@
 public class MyList implements IntegerList {
+    private int size;
+    private int capacity;
+    private Integer[] myList;
+
+
+    public MyList() {
+        size = 0;
+        capacity = 4;
+        myList = new Integer[capacity];
+    }
+
+    public MyList(int size) {
+        this.size = size;
+        capacity = 2 * size;
+        myList = new Integer[capacity];
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        capacity *= 2;
+        Integer[] newMyList = new Integer[capacity];
+        for (int i = 0; i < size; i++) {
+            newMyList[i] = myList[i];
+        }
+        myList = newMyList;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (o == myList[i]) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public Integer get(int index) {
-        return null;
+        if (index >= size) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            return myList[index];
+        }
     }
 
     @Override
     public void add(int index, Integer element) {
+        if (index > size){
+            throw new IndexOutOfBoundsException();
+        }
 
+        if (size == capacity) {
+            resize();
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            myList[i + 1] = myList[i];
+        }
+        myList[index] = element;
+        size += 1;
     }
 
     @Override
     public Integer remove(int index) {
-        return null;
+        if (index > size){
+            throw new IndexOutOfBoundsException();
+        }
+        Integer number = myList[index];
+
+        for (int i = index + 1; i < size; i++) {
+            myList[i - 1] = myList[i];
+        }
+        size -= 1;
+        return number;
     }
 
     @Override
     public int indexOf(Integer o) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (o == myList[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public boolean add(Integer e) {
-        return false;
+        add(size, e);
+        return true;
     }
 
     @Override
     public boolean remove(Integer o) {
-        return false;
+        int index = indexOf(o);
+        if (index == -1) {
+            return false;
+        }
+        remove(index);
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder myListString = new StringBuilder();
+        myListString.append("[");
+
+        for (int i = 0; i < size; i++) {
+            if (i == size - 1) {
+                myListString.append(String.format("%d", myList[i]));
+
+            }else {
+                myListString.append(String.format("%d, ", myList[i]));
+            }
+        }
+
+        myListString.append("]");
+        return myListString.toString();
+    }
+
+    public void print() {
+        System.out.println(this);
     }
 }
