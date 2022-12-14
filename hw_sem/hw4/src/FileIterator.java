7e8a169289c149af35bb85c8674d78677be306e1
thendray <panda8o7@yandex.ru>
2022-12-14 19:23:46 +0300

    doc

--- hw_sem/hw4/src/FileIterator.java
+++ hw_sem/hw4/src/FileIterator.java
@@ -5,6 +5,12 @@ class FileIterator implements Iterator<String> {
     private String next_line;
     private final BufferedReader BUFFER_READER;
 
+    /**
+     * Create a buffer reader if all good with path to file, else throw FileNotFoundException
+     * Then get a first line in file
+     * @param path path to file
+     * @throws IOException FileNotFound if smth wrong with path and IOEcxeption if smth wrong with readLine from buffer
+     */
     public FileIterator(String path) throws IOException {
         File file = new File(path);
 
@@ -16,11 +22,19 @@ class FileIterator implements Iterator<String> {
         next_line = BUFFER_READER.readLine();
     }
 
+    /**
+     * Check has the buffer one more line
+     * @return true if buffer has new line, else return false
+     */
     @Override
     public boolean hasNext() {
         return next_line != null;
     }
 
+    /**
+     * Get next line from buffer
+     * @return String if all is good or throw RunTimeException if smth goes wrong with readline from buffer
+     */
     @Override
     public String next() {
         String next_line = this.next_line;
